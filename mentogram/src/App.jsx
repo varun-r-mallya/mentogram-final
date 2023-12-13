fe97baa85da4e62e8e0e9dd3c6a2ac2816f398d1
Varun R Mallya <varunrmallya@gmail.com>
2023-12-14 01:09:51 +0530

    lint

--- mentogram/src/App.jsx
+++ mentogram/src/App.jsx
@@ -8,3 +8,13 @@ export default function App() {
     </div>
   );
 }
+
+
+// check for cookies here
+// if cookies exist, render the main page
+// check if valid cookies exist. If not, render the login page
+// if cookies do not exist, render the login page
+// keyword: Conditional routing
+// check for cookies on frontend 
+// create function to check for cookies on every render of page and redirect to login page if cookies do not exist
+// useEffect for single render per redirect. 
