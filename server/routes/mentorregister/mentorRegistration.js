02d053764f743450955a14e6078eec3ad7bd4486
Varun R Mallya <varunrmallya@gmail.com>
2023-12-15 12:46:46 +0530

    added user already registered

--- server/routes/mentorregister/mentorRegistration.js
+++ server/routes/mentorregister/mentorRegistration.js
@@ -3,7 +3,6 @@ exports.mentorRegistration = function (username, email, hashedpassword, res)
     const mongoose = require('mongoose');
     const registeredMentors = require('./mentorregistrationschema');
 
-
     mongoose.connect('mongodb://127.0.0.1:27017/testdb')
         .then(() => {
             console.log('Successfully connected to MongoDB');
@@ -12,16 +11,27 @@ exports.mentorRegistration = function (username, email, hashedpassword, res)
             console.error('Failed to connect to MongoDB:', error);
         });
 
-    const user = new registeredMentors({username: username, email: email, hashedPassword: hashedpassword});
-    // check if same usernamae and email another user is stored or not
-    user.save()
-        .then(() => {
-            console.log('Mentor registered successfully')
-            res.status(200).json({ message: 'Registration successful' });
-            
+    registeredMentors.findOne({ $or: [{ username: username }, { email: email }] })
+        .then((existingUser) => {
+            if (existingUser) {
+                console.log('User already exists');
+                res.status(400).json({ message: 'User already exists' });
+                return;
+            }
+
+            const user = new registeredMentors({username: username, email: email, hashedPassword: hashedpassword});
+            user.save()
+                .then(() => {
+                    console.log('Mentor registered successfully')
+                    res.status(200).json({ message: 'Registration successful' });
+                })
+                .catch((error) => {
+                    console.error('Failed to register Mentor:', error);
+                    res.status(400).json({ message: 'Invalid credentials' });
+                });
         })
         .catch((error) => {
-            console.error('Failed to register Mentor:', error);
-            res.status(400).json({ message: 'Invalid credentials' });
+            console.error('Failed to check existing user:', error);
+            res.status(500).json({ message: 'Internal server error' });
         });
 }
