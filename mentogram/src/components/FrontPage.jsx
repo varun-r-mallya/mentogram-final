3ab86ca4602ee12d9225561f122856a7fa76a5fe
Varun R Mallya <varunrmallya@gmail.com>
2023-12-17 13:34:19 +0530

    made homepage spicy

--- mentogram/src/components/FrontPage.jsx
+++ mentogram/src/components/FrontPage.jsx
@@ -4,21 +4,9 @@ import logoformentogram2 from '../assets/logoformentogram2.png';
 import { useState, useEffect, useRef } from 'react';
 import MentorLogin from './MentorLogin.jsx';
 import MenteeLogin from './MenteeLogin.jsx';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import AuthChecker from './AuthChecker';
-// import HomePage from './HomePage';
-// import MentorControl from '../protectedcomponents/mentor/MentorControl';
-// import MenteeFrontPage from '../protectedcomponents/mentee/MenteeFrontPage';
 
 export default function FrontPage(props) {
   const [ choice, setChoice ] = useState('null');
-  // const [isSignedIn, setIsSignedIn] = useState(null)
-  // const signin = () => {
-  //   setIsSignedIn(true)
-  // }
-  // const signout = () => {
-  //   setIsSignedIn(false)
-  // }
 
   const display = () => {
     if (choice === 'null') {
@@ -54,4 +42,4 @@ export default function FrontPage(props) {
       <footer>Made by Varun R Mallya, as the Winter Of Code project under SDSLabs-IITR</footer>
     </div>
   );
-}
+} 
--- mentogram/src/components/HomePage.jsx
+++ mentogram/src/components/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import DarkModeSwitch from './DarkModeSwitch.jsx';
+import logoformentogram2 from '../assets/logoformentogram2.png';
+
 export default function HomePage(){
     const navigate = useNavigate();
     
     return(
         <div>
+            <header onClick={() => setChoice("null")}><img src={logoformentogram2} alt="Mentogram Logo" className="logo" /></header>
+            <DarkModeSwitch />
             <h1>HomePage</h1>
             <Link to="/login">Login</Link>
+            <footer>Made by Varun R Mallya, as the Winter Of Code project under SDSLabs-IITR</footer>
         </div>
     );
 }
