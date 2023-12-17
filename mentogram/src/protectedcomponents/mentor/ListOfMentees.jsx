527a946801493b3ff6752122a670213ef33014e9
Varun R Mallya <varunrmallya@gmail.com>
2023-12-17 14:10:41 +0530

    made infrastucture for adding list of mentees

---
+++ mentogram/src/protectedcomponents/mentor/ListOfMentees.jsx
@@ -0,0 +1,15 @@
+import AddMentees from "./AddMentees.jsx";
+
+// this function is going to use the mentees array in the database under the mentor and render the mentees in a list with a TO-DO lsit like
+// fashion. There will be tiny dustbins as delete buttons. There will be a button to add mentees. Which will redirect the mentor to the
+// AddMentees.jsx. There will also be a button on each mentee to "Connect mentee". This will redirect the mentor to the collaborative coding
+// /chat/videochat/filemanager page. 
+
+export default function ListOfMentees(){
+    return(
+        <div>
+            <h1>List Of Mentees</h1>
+
+        </div>
+    );
+}
